100131292422c5308dc5d7dc40de6e5ce32b32c8
TrevorBrowning <trevryanbrowning@gmail.com>
2025-06-21 05:21:39 -0400

    feat:Update-css-Add-Paste-Func

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-const canvas = document.getElementById("canvas");
+addEventListener("paste", (event) => {
+  event.preventDefault();
+
+  const items = event.clipboardData.items;
+
+  let imageFile = null;
+
+  for (const item of items) {
+    if (item.kind === "file" && item.type.startsWith("image/")) {
+      imageFile = item.getAsFile();
+
+      break;
+    }
+  }
+
+  if (imageFile) {
+    console.log("Successfully captured image file:", imageFile);
+    const storedURL = URL.createObjectURL(imageFile);
+    const image = new Image();
+    image.onload = () => {
+      console.log("Image has loaded and is ready to be drawn!");
+      const canvas = document.getElementById("image-paste");
+      const context = canvas.getContext("2d");
+      canvas.width = image.width;
+      canvas.height = image.height;
+      context.drawImage(image, 0, 0);
+    };
+    image.src = storedURL;
+  }
+});
